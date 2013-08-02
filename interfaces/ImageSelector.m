25d2fb54ff2633c8aaf5c0c3f8d833af118a0a50
Michael Porter <m.porter@dundee.ac.uk>
2013-08-02 17:13:13 +0100

    Writing a test comment

--- interfaces/ImageSelector.m
+++ interfaces/ImageSelector.m
@@ -51,6 +51,7 @@ function ImageSelector_OpeningFcn(hObject, eventdata, handles, varargin)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to ImageSelector (see VARARGIN)
+%TestComment
 
 % Choose default command line output for ImageSelector
 handles.output = hObject;
