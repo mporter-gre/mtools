3788d8be3f74972b4a7c7aec37bf2cecf0603269
mporter-gre <m.porter@dundee.ac.uk>
2014-03-04 11:42:36 +0000

    cellProps

--- cellPropsToXLS.m
+++ cellPropsToXLS.m
@@ -3,15 +3,15 @@ function cellData = cellPropsToXLS(cellProps, imageName)
 numCells = length(cellProps);
 rowCounter = 1;
 cellData = {};
-cellData = {'Image Name', 'Cell Num', 'Num Neighbours', 'Num Foci', 'Neighbour Distance'};
+%cellData = {'Image Name', 'Cell Num', 'Num Neighbours', 'Num Foci', 'Neighbour Distance'};
 for thisCell = 1:numCells
     numNeighbours = cellProps{thisCell}.numNeighbours;
     if numNeighbours > 0
         for thisNeighbour = 1:numNeighbours
-            cellData = [cellData; {imageName, thisCell, numNeighbours, cellProps{thisCell}.numFoci, cellProps{thisCell}.neighbourDist(thisNeighbour)}];
+            cellData = [cellData; {0, thisCell, numNeighbours, cellProps{thisCell}.numFoci, cellProps{thisCell}.neighbourDist(thisNeighbour)}];
         end
     else
-        cellData = [cellData; {imageName, thisCell, numNeighbours, cellProps{thisCell}.numFoci, 0}];
+        cellData = [cellData; {0, thisCell, numNeighbours, cellProps{thisCell}.numFoci, 0}];
     end
    
     
