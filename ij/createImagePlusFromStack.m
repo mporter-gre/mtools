09505a33f56cd391923cb8c623b676252ba668fd
mporter-gre <m.porter@dundee.ac.uk>
2014-06-25 15:00:33 +0100

    ij ImagePlus

--- ij/createImagePlusFromStack.m
+++ ij/createImagePlusFromStack.m
@@ -16,9 +16,20 @@ end
 
 imageStack = ij.ImageStack(sizeX,sizeY);
 for thisPlane = 1:numPlanes
-    bytesOut = toByteArray(stackIn(:,:,thisPlane), pixels);
-    imageStack.addSlice('',bytesOut);
+    %bytesOut = toByteArray(stackIn(:,:,thisPlane), pixels);
+    byteArray = reshape(stackIn(:,:,thisPlane)', sizeX * sizeY, 1 );
+    imageStack.addSlice('',byteArray);
 end
 
 imp = ij.ImagePlus();
 imp.setStack(imageStack, numC, numZ, numT);
+
+width = pixels.getPhysicalSizeX.getValue;
+height = pixels.getPhysicalSizeY.getValue;
+depth = pixels.getPhysicalSizeZ.getValue;
+
+calibration = ij.measure.Calibration();
+calibration.pixelWidth = width;
+calibration.pixelHeight = height;
+calibration.pixelDepth = depth;
+imp.setCalibration(calibration);
